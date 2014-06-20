f211bae5c8aca2f46c61ec5648f340132f611728
spyderwebr <rebbewmr@gmail.com>
2014-06-20 00:01:33 -0400

    Submitting assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## make cache matrix function
+## set matrix data, initial cache value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	invMat <- matrix(data = NA)  		## initialize invMat to NA
+	set <- function(y) {
+		x <<- y
+		invMat <<- matrix(data = NA)  ## cache NA value for invMat
+	}
+	get <- function() x
+	setinvMat <- function(solve) invMat <<- solve
+	getinvMat <- function() invMat
+	list (set = set, get = get, setinvMat = setinvMat,
+		getinvMat = getinvMat)
 }
 
+## inverse matrix cache function
+## report solve(): calculate if not cached, else report cached value
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+	invMat <- x$getinvMat()  	## retrieve cached solve() value from makeCacheMatrix()
+	if(!is.na(invMat)) {	 	## determine if cache contains solved value
+		message("retrieved cached solve() results")
+		return(invMat)		## return cached value if available
+	}
+	data <- x$get()
+	invMat <- solve(data, ...)	## determine solve() if NA cached
+	x$setinvMat(invMat)		## set determined solve() in cache
+	invMat				## return solve()
 }
+
